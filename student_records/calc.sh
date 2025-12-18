2fa24d6988d6fa2d0fadf465c2363e6a6033ac03
ajaytanguturi <tanguturiajay5872@gmail.com>
2025-12-18 13:26:50 +0530

    first commit

---
+++ student_records/calc.sh
@@ -0,0 +1,14 @@
+echo " enter 1st number"
+read num1
+echo " enter 2nd number"
+read num2
+sum=$((num1+num2))
+difference=$((num1-num2)) 
+prod=$((num1*num2))
+div=$((num1/num2))
+
+echo "sum is : $sum"
+echo "diff is : $diff"
+echo "product is : $prod"
+echo "quotient is : $div"
+
